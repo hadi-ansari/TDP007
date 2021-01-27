633f73e4b27381928c3c9aa89f5f6b723d3d5a4c
Vincent Ahlström <vinah331@student.liu.se>
2021-01-27 11:48:20 +0100

    Update upg2.rb

--- Lab2/upg2.rb
+++ Lab2/upg2.rb
@@ -1,7 +1,7 @@
 require "rexml/document"
 
-class Event
-  attr_reader :event_hash
+class Event # Creates Event Object (hash)
+  attr_reader :event_hash #getter
   def initialize()
     @event_hash = Hash.new
   end
@@ -23,17 +23,17 @@ def xhtml_read(website)
   src = File.open website
   doc = REXML::Document.new src
   event_list = Array.new
-  doc.elements.each("//div[@class='vevent']") do |element|
-    event = Event.new
+  doc.elements.each("//div[@class='vevent']") do |element| # Loops through all paths following div class named 'vevent'
+    event = Event.new # Event object
     event.add_field("Name", element.elements[".//span[@class='summary']"].text)
     event.add_field("Date", element.elements[".//span[@class='dtstart']"].text)
     location = ""
-    element.elements.each(".//td[@class='location']//")  do |l|
+    element.elements.each(".//td[@class='location']//")  do |l| # Finds all location data and combines it to a string
       if (l.has_text?) && (l.text.gsub(/\n/,'').strip != "")
         location += "#{l.text}, "
       end
     end
-    event.add_field("Location", location[0...-2])
+    event.add_field("Location", location[0...-2]) #städar upp location och lägger till i event object
     
     event.add_field("Description", element.elements[".//td[@class='description']/p"].text)
     event.add_field("Posted by", element.elements[".//a[@class='userLink ']"].text)
@@ -50,7 +50,7 @@ def xhtml_read(website)
  return event_list
 end
 
-def print_events(event_list)
+def print_events(event_list) # Skriver ut varje event ur listan
   for e in (0...event_list.size)
   puts "Event #{e + 1}\n#{"="*8}\n#{event_list[e]}\n"
 end
